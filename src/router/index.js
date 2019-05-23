321a2d425407e89bdc41dfc320411dd25645c8ec
Jerome <jerome@spinprotocol.io>
2019-05-23 09:04:29 +0900

    Implement Vue Router

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import AskView from '../views/AskView';
+import JobsView from '../views/JobsView';
+import NewsView from '../views/NewsView';
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  routes: [
+    {
+      path: '/news',
+      component: NewsView,
+    },
+    {
+      path: '/ask',
+      component: AskView,
+    },
+    {
+      path: '/jobs',
+      component: JobsView,
+    },
+  ],
+});
